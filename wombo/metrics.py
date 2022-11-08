714de2672ca6556ee190ef935e6a13a3cd405670
Amrit Parmanand <amritparmanand@gmail.com>
2022-11-08 11:56:50 -0500

    making things less complicated

--- wombo/metrics.py
+++ wombo/metrics.py
@@ -3,17 +3,14 @@ from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
 from opentelemetry.exporter.otlp.proto.grpc.metric_exporter import OTLPMetricExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-
-from os import getenv
-OTEL_SERVICE_NAME = getenv('OTEL_SERVICE_NAME', 'trimmed-fastapi')
-OTEL_EXPORTER_OTLP_ENDPOINT = getenv('OTEL_EXPORTER_OTLP_ENDPOINT', 'http://localhost:4317')
+from opentelemetry.sdk.environment_variables import OTEL_SERVICE_NAME, OTEL_EXPORTER_OTLP_ENDPOINT
 
 resource = Resource(attributes={
     SERVICE_NAME: OTEL_SERVICE_NAME
 })
 reader = PeriodicExportingMetricReader(OTLPMetricExporter(endpoint=OTEL_EXPORTER_OTLP_ENDPOINT))
 metrics.set_meter_provider(MeterProvider(resource=resource, metric_readers=[reader]))
-meter = metrics.get_meter("worker_vqgan")
+meter = metrics.get_meter("worker_vqgan", "0.1.0")
 
 requests_counter = meter.create_counter(
     name="requests",
